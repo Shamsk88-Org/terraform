fccb9e7b4e6f9c42fc232d20f894646d0831840e
Shamsher Khan <shamsk88@gmail.com>
2025-04-11 13:06:10 +0530

    added subscription ID

--- backend.tf
+++ backend.tf
@@ -2,11 +2,11 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "your_resource_group_name"
-    storage_account_name = "your_storage_account_name"
-    container_name       = "your_container_name"
-    key                  = "your_tfstate_file.tfstate"
-    tenant_id            = "your_tenant_id"
-    subscription_id      = "your_subscription_id"
+    resource_group_name  = "rg-dev-app-002"
+    storage_account_name = "DevOps_Dev2025"
+    container_name       = "Development"
+    key                  = "Dev_tfstate.tfstate"
+    tenant_id            = "648b628e-c17f-4d43-8193-72e3f4d7499d"
+    subscription_id      = "24143a81-fcba-4b21-9b28-c910b75e6580"
   }
 }
