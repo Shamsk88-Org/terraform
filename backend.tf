64741b3c0ba6f2693b3295fcae9c08fdbc9f2ee9
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-13 16:22:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-dev-app-001"
     storage_account_name = "devopsdev2025"
     container_name       = "development"
-    key                  = "dev_tfstate.tfstate"
+    key                  = "devterraform.tfstate"
     tenant_id            = "648b628e-c17f-4d43-8193-72e3f4d7499d"
     subscription_id      = "24143a81-fcba-4b21-9b28-c910b75e6580"
   }
