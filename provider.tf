0273764bb7901ab36e7d7b571038ab6260ccb5d2
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-14 16:44:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+ provider "azurerm" {
+   features {}
+
+   subscription_id = "24143a81-fcba-4b21-9b28-c910b75e6580"
+   tenant_id       = "648b628e-c17f-4d43-8193-72e3f4d7499d"
+   use_msi         = true
+   client_id       = "1ac098dd-90b5-4054-80e7-8baf99b04f64"
+ }
